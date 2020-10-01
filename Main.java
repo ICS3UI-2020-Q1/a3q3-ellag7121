8b0488d7a941a5df7c3f6e0ce3ba531b7f3e8e14
ellag7121 <ellag7121@wrdsb.ca>
2020-10-01 15:22:04 +0000

    finished a3q3

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * get a letter grade from the user, tell them the grade as a percent
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,32 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner to get input
+    Scanner input = new Scanner(System.in);
+
+    //ask for letter grade
+    System.out.println("Please enter a letter grade");
+    String grade = input.nextLine(); //store it in a variable
+
+    //check what grade was entered and give the corresponding percentage
+    switch(grade){
+      case "A": //if grade is A
+        System.out.println("This is between 80% and 100%"); //it is between 80-100
+        break;
+      case "B": //if grade is B
+        System.out.println("This is between 70% and 79%"); //it is between 70-79
+        break;
+      case "C": //if grade is C
+        System.out.println("This is between 60% and 69%"); //it is between 60-69
+        break;
+      case "D": //if grade is D
+        System.out.println("This is between 50% and 59%"); //it is between 50-59
+        break;
+      case "F": //if grade is F
+        System.out.println("This is between 0% and 49%"); //it is between 0-49
+    }
+
+
     
   }
 }
